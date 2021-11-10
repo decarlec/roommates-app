ad86a789e6a00722e6bd9d61bb50d7a305a5fb47
decarlec <decarle.c@gmail.com>
2021-11-09 21:59:18 -0500

    Got tailwind styling working:

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -5,7 +5,7 @@ import { TASKS } from '../mock-tasks';
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
 
