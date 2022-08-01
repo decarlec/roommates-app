283c14f1b74ae60559ed19e01cb0ffe354429090
decarlec <decarle.c@gmail.com>
2022-07-31 21:34:02 -0400

    fixed styling

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatCardModule} from '@angular/material/card';
-import { MatListModule} from '@angular/material/list';
-import { MatSidenavModule} from '@angular/material/sidenav';
-import { MatToolbarModule} from '@angular/material/toolbar';
-import { MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @NgModule({
-  imports: [MatCheckboxModule, MatCardModule, MatListModule, BrowserModule, BrowserAnimationsModule, MatSidenavModule, MatToolbarModule, MatIconModule],
-  exports: [MatCheckboxModule, MatCardModule, MatListModule, BrowserModule, BrowserAnimationsModule, MatSidenavModule, MatToolbarModule, MatIconModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatListModule, BrowserModule, BrowserAnimationsModule, MatSidenavModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatListModule, BrowserModule, BrowserAnimationsModule, MatSidenavModule, MatToolbarModule, MatIconModule],
 })
 
-export class MaterialModule {}
+export class MaterialModule { }
